21db926420b220479c2599173e0997fd692b1b79
John Castro <johhncaastro@gmail.com>
2023-02-10 22:26:59 -0600

    fully functional with assets still needs random feature to work

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-//todo: assets need to be made for this block
+// Character image
+const characterImg = new Image();
+characterImg.src = "block.png";
+
 // Character properties
 const character = {
     x: 50,
     y: canvas.height - 150,
     width: 100,
     height: 100,
-    color: "black",
     jump: false
 };
 
-//todo: make assets for the objects coming in and make a random script to make the game harder + work on Grav
+// Block image
+const blockImg = new Image();
+blockImg.src = "character.png";
+
 // Block properties
 const block = {
     x: canvas.width,
-    y: canvas.height - 50,
+    y: canvas.height - 200,
     width: 50,
     height: 50,
-    color: "red",
     speed: 5
 };
 
@@ -43,49 +47,57 @@ function checkCollision(char, obj) {
     let objTop = obj.y;
     let objBottom = obj.y + obj.height;
 
-    return charLeft < objRight && charRight > objLeft && charTop < objBottom && charBottom > objTop;
-
+    if (charRight > objLeft && charLeft < objRight && charBottom > objTop && charTop < objBottom) {
+        alert("Game Over!");
+        document.location.reload();
+    }
 }
 
 // Draw function
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = character.color;
-    ctx.fillRect(character.x, character.y, character.width, character.height);
 
-    ctx.fillStyle = block.color;
-    ctx.fillRect(block.x, block.y, block.width, block.height);
+    // Draw block
+    ctx.drawImage(blockImg, block.x, block.y, block.width, block.height);
 
-    // Move character up and down when jumping
+    // Move block
+    block.x -= block.speed;
+    if (block.x + block.width < 0) {
+        block.x = canvas.width;
+    }
+
+    // Check collision
+    checkCollision(character, block);
+
+    // Draw character
+    ctx.drawImage(characterImg, character.x, character.y, character.width, character.height);
+
+    // Jump
     if (character.jump) {
         character.y -= 20;
     } else {
         character.y += 20;
     }
 
-    // Keep character within canvas bounds
-    if (character.y > canvas.height - character.height) {
+    // Keep character within canvas
+    if (character.y + character.height > canvas.height) {
         character.y = canvas.height - character.height;
-    } else if (character.y < 0) {
+    }
+    if (character.y < 0) {
         character.y = 0;
     }
 
-    // Move block left
-    block.x -= block.speed;
-
-    // Reset block position if it goes off canvas
-    if (block.x + block.width < 0) {
-        block.x = canvas.width;
-    }
-
-    // Check for collision
-    if (checkCollision(character, block)) {
-        alert("Game Over");
-    }
     requestAnimationFrame(draw);
 }
 
-canvas.addEventListener("click", jump);
+// Listen for jump
+document.addEventListener("keydown", event => {
+    if (event.code === "Space") {
+        jump();
+    }
+});
+
+// Start game
 draw();
 
 
